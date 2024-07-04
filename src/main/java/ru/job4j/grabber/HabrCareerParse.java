f23ee21ac85d3d7bebe0732823edfddf65532845
leofinder <imrhyde@yandex.ru>
2024-07-04 19:24:40 +0500

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,9 +16,15 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int PAGE_COUNT = 5;
 
     public static void main(String[] args) throws IOException {
-        int pageNumber = 1;
+        for (int i = 1; i <= PAGE_COUNT; i++) {
+            parseVacancyCards(i);
+        }
+    }
+
+    private static void parseVacancyCards(int pageNumber) throws IOException {
         String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
         Connection connection = Jsoup.connect(fullLink);
         Document document = connection.get();
