615d2d808d649b6c8fde25f6e5cd28542b969db0
leofinder <imrhyde@yandex.ru>
2024-07-06 18:30:10 +0500

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,9 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -16,13 +19,36 @@ public class Grabber implements Grab {
     private final Parse parse;
     private final Store store;
     private final Scheduler scheduler;
+    private final Properties cfg;
     private final int time;
 
-    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
+    public Grabber(Parse parse, Store store, Scheduler scheduler, Properties cfg, int time) {
         this.parse = parse;
         this.store = store;
         this.scheduler = scheduler;
+        this.cfg = cfg;
         this.time = time;
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     @Override
@@ -65,6 +91,8 @@ public class Grabber implements Grab {
         var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         var store = new PsqlStore(config);
         var time = Integer.parseInt(config.getProperty("time"));
-        new Grabber(parse, store, scheduler, time).init();
+        var grab = new Grabber(parse, store, scheduler, config, time);
+        grab.init();
+        grab.web(store);
     }
 }
